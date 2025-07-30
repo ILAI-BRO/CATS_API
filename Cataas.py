cc47465c92f1c7405fc181776eef9e316d4bb2dc
ilas <ayderkhanov@yandex.ru>
2025-07-30 22:48:35 +0300

    add file menu rather button

--- Cataas.py
+++ Cataas.py
@@ -3,17 +3,24 @@ from PIL import ImageTk, Image
 import requests
 from io import BytesIO
 
+url = "https://cataas.com/cat"
+
+
+# Получаем изображение
 def get_image(url):
     try:
         response = requests.get(url)
         response.raise_for_status()
         img_data = BytesIO(response.content)
-        img = Image.open(img_data).resize((600, 480))
+        img = Image.open(img_data)
+        img.thumbnail((600, 480),  Image.Resampling.LANCZOS)
         return ImageTk.PhotoImage(img)
     except Exception as error:
         print(f"Произошла ошибка: {error}")
         return None
 
+
+# Изменяем изображение на новое
 def change_img():
     img = get_image(url)
 
@@ -25,13 +32,23 @@ root = Tk()
 root.title("Cats Images")
 root.geometry("600x530")
 
+
+
 label =Label()
 label.pack()
 
-btn_change = Button(text="Обнови котика", command=change_img)
-btn_change.pack()
+# btn_change = Button(text="Обнови котика", command=change_img)
+# btn_change.pack()
 
-url = "https://cataas.com/cat"
+
+menu_bar = Menu(root)
+root.config(menu=menu_bar)
+
+file_menu = Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="Файл", menu=file_menu)
+file_menu.add_command(label="загрузить фото", command=change_img)
+file_menu.add_separator()
+file_menu.add_command(label="Выход", command=root.quit)
 
 change_img()
 
