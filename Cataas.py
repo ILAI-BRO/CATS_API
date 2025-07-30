60fbc33c0a45ca7f0356a60eb0818fea6695f6c8
ilas <ayderkhanov@yandex.ru>
2025-07-30 22:15:02 +0300

    Creat window and widgets

--- Cataas.py
+++ Cataas.py
@@ -3,6 +3,20 @@ from PIL import ImageTk, Image
 import requests
 from io import BytesIO
 
+def get_image():
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        img_data = BytesIO(response.content)
+        img = Image.open(img_data)
+        return ImageTk.PhotoImage(img)
+    except Exception as error:
+        print(f"Произошла ошибка: {error}")
+        return None
+
+
+
+
 root = Tk()
 root.title("Cats Images")
 root.geometry("600x480")
@@ -16,12 +30,5 @@ img = get_image(url)
 if img:
     label.config(image=img)
     label.image = img
-
-def get_image(url):
-    response = requests.get(url)
-    image = Image.open(BytesIO(response.content))
-    image = image.convert("RGB")
-    return image
-
 
 root.mainloop()
